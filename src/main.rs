f061d70a2a1c9bb7b07f6b8f3fe0af49f9257242
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-06-01 17:45:36 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-//num-bigint 및 num-integer를 기반으로 하는 암호학적으로 안전한 소수 생성기
-extern crate glass_pumpkin;
-
+extern crate rsa;
+use std::io::{BufReader,BufRead,BufWriter,Write,stdout,stdin};
+use rsa::{PublicKey,RSAPrivateKey,PaddingScheme};
+use rand::rngs::OsRng;
 fn main() {
+
+    let mut reader= BufReader::new(stdin().lock());
+    let mut input= String::new();
+    reader.read_line(&mut input).unwrap();
+    let s= input.trim().to_string();
     println!("Hello, world!");
 }
+
+//N=PQ
+//Pub= (e,N)
+//Priv=(d,N)
+
+
+//Enc=M^e (mod N)
+
+
+//Dec=Enc^d (modN)
+
+
